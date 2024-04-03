82e8f7c29329828ce58c48a4bf0db67edd491d66
Gospel011 <ugochukwugospeli111@gmail.com>
2024-04-03 17:10:48 +0100

    Initialized project skeleton

---
+++ themes_app/lib/util/constants.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  //? Light theme
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  //? Dark theme
+  static final Color darkGrey = Colors.grey[900]!;
+  static const Color lightGrey = Colors.grey;
+}
